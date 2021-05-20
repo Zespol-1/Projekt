4305e7e14e82379bd1607d9ab837d12222c37742
C4mi <56874699+C4mi@users.noreply.github.com>
2021-05-20 13:14:57 +0200

    Add files via upload
    Fast modular exponentiation

---
+++ bin/potegowanie.cpp
@@ -0,0 +1,44 @@
+﻿// potegowanie.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+unsigned long long power(unsigned long long x, unsigned long long y, unsigned long long n)
+{
+    int res = 1;     // Initialize result
+
+    while (y > 0)
+    {
+        // If y is odd, multiply x with result
+        if (y & 1)
+            res = res*x%n;
+
+        // y must be even now
+        y = y >> 1; // y = y/2
+        x = x*x%n;  // Change x to x^2
+    }
+    return res;
+}
+
+
+void tests() {
+}
+
+
+
+int main()
+{
+
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
