dece21a5171a61dbf38e3dce821637586542b429
kaniad99 <dominik.kania100@gmail.com>
2021-05-21 14:54:35 +0200

    Szybkie potęgowanie

--- bin/potegowanie.cpp
+++ bin/potegowanie.cpp
@@ -3,42 +3,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <iostream>
+#include <cassert>
 
-unsigned long long power(unsigned long long x, unsigned long long y, unsigned long long n)
+using namespace std;
+const unsigned long long p = 9223372036854775783;
+
+uint64_t product_mult32_reduce(uint64_t tbr) {
+    tbr = tbr % p;
+    uint64_t x1 = tbr >> 32;
+    uint64_t y1 = tbr & 0xffffffff;
+    uint64_t res = ((y1 << 32) % p) + x1 * 50;
+    return res % p;
+}
+
+uint64_t product_mult64_reduce(uint64_t tbr) {
+    tbr = tbr % p;
+    uint64_t x1 = tbr >> 32;
+    uint64_t y1 = tbr & 0xffffffff;
+    uint64_t res = product_mult32_reduce(x1 * 50) + y1 * 50;
+    return res % p;
+}
+
+
+unsigned long long mult(unsigned long long a, unsigned long long b) {
+
+
+    unsigned long long x1 = a;
+    x1 = x1 >> 32;
+    unsigned long long y1;
+    y1 = a & 0xffffffff;
+    unsigned long long x2 = b;
+    x2 = x2 >> 32;
+    unsigned long long y2;
+    y2 = b & 0xffffffff;
+
+
+    uint64_t halfa = product_mult64_reduce(x1 * x2);
+    uint64_t halfb = product_mult32_reduce(x1 * y2);
+    uint64_t halfc = product_mult32_reduce(x2 * y1);
+    uint64_t halfd = (y1 * y2) % p;
+
+
+    return ((((halfa + halfb) % p + halfc) % p + halfd) % p);
+}
+
+unsigned long long power(unsigned long long x, unsigned long long y)
 {
-    int res = 1;     // Initialize result
+    unsigned long long res = 1;     // wynik
 
     while (y > 0)
     {
-        // If y is odd, multiply x with result
+        // Jesli y jest nieparzyste, mnozy x z wynikiem
         if (y & 1)
-            res = res*x%n;
+            res = mult(res, x);
 
-        // y must be even now
-        y = y >> 1; // y = y/2
-        x = x*x%n;  // Change x to x^2
+        // y musi byc parzyste
+        y = y >> 1; // podziel y = y/2
+        x = mult(x,x);  // Zmien x na x^2
     }
     return res;
 }
 
 
 void tests() {
-}
 
+
+}
 
 
 int main()
 {
-
+    cout << power(2138,696969);
 }
-
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
